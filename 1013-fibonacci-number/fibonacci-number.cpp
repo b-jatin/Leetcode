7054fcc7a0c5417f83c1d6e7a099105f76662c18
b-jatin <59656324+b-jatin@users.noreply.github.com>
2025-03-09 19:12:35 -0700

    Time: 0 ms (100.00%) | Memory: 7.6 MB (86.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,6 +3,13 @@ public:
     int fib(int n) {
         if(n==0 || n==1)
             return n;
-        return fib(n-1) + fib(n-2);
+        int a=0,b=1,temp=0;
+        for(int i=0;i<n-1;i++){
+            temp=b+a;
+            a=b;
+            b=temp;
+
+        }
+        return b;
     }
 };
