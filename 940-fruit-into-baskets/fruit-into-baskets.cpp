464a9842de27d8fe52fc7598021a9139bd075119
b-jatin <59656324+b-jatin@users.noreply.github.com>
2025-03-10 18:54:48 -0700

    Time: 40 ms (60.45%) | Memory: 75.2 MB (34.18%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -4,10 +4,10 @@ public:
         unordered_map<int,int> temp;
         int start=0,end=0, len=fruits.size(),maxf=INT_MIN;
         while(end<len){
-            if(temp.count(fruits[end])==0)
-            temp.insert({fruits[end],0});
+           // if(temp.count(fruits[end])==0)
+           // temp.insert({fruits[end],0});
             temp[fruits[end]]++;
-            while(temp.size()>2){
+            if(temp.size()>2){
                 temp[fruits[start]]--;
                 if(temp[fruits[start]]==0)
                     temp.erase(fruits[start]);
