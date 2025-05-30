5d8f809f2c201331225b72d761e39ed3668b2802
b-jatin <59656324+b-jatin@users.noreply.github.com>
2025-05-29 18:46:47 -0700

    Time: 0 ms (100.00%) | Memory: 10.6 MB (79.69%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> temp(n+1,0);
+        
+        for(int i=1;i<=n;i++){
+            if(i%2==1){
+                temp[i]= temp[i/2]+1;
+            }
+            else
+            temp[i]=temp[i/2];
+           
+        }
+        return temp;
+    }
+};
