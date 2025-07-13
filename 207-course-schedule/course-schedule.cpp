f32b90569b1cab546efc5b60139cb1cb9a65d528
b-jatin <59656324+b-jatin@users.noreply.github.com>
2025-07-12 19:35:05 -0700

    Time: 11 ms (22.16%) | Memory: 20.2 MB (31.39%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    bool dfs(int start,vector<vector<int>>& adj,vector<int> & visited, vector<int>& path,int n){
-        visited[start]=1;
-        path[start]=1;
-        for(auto x: adj[start]){
-            if(!visited[x]){
-               if( dfs(x,adj,visited,path,n)==false)
-                    return false;
-            }
-            else if(path[x])
-                return false;
-        }
-        path[start]=0;
-        return true;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for(auto i: prerequisites){
             adj[i[1]].push_back(i[0]);
         }
-        vector<int> visited(numCourses,0), path(numCourses,0);
+        vector<int> indegree(numCourses,0);
         for(int i=0;i<numCourses;i++){
-                if(dfs(i,adj,visited,path,numCourses)==false)
-                    return false;
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
         }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int count=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            count++;
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(count==numCourses)
         return true;
+        return false;
     }
 };
