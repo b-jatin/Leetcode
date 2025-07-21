f04f26d5b588467941bb5c5fa0a338c7b4ef3822
b-jatin <59656324+b-jatin@users.noreply.github.com>
2025-07-20 19:47:57 -0700

    Time: 25 ms (52.20%) | Memory: 139.2 MB (35.88%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -3,23 +3,16 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
          vector<int> res;
-        deque<int> deque;
-
-        for (int idx = 0; idx < nums.size(); idx++) {
-            int num = nums[idx];
-
-            while (!deque.empty() && deque.back() < num) {
-                deque.pop_back();
-            }
-            deque.push_back(num);
-
-            if (idx >= k && nums[idx - k] == deque.front()) {
-                deque.pop_front();
-            }
-
-            if (idx >= k - 1) {
-                res.push_back(deque.front());
-            }
+        deque<int> dq;
+        //striver solution
+        for (int i = 0; i < nums.size(); i++) {//storing indexes in deque. pushing from back(monotonic decreasing)
+            if(!dq.empty() && dq.front()<=i-k)  // maintaining k elements window(removing the unwanted elements)
+                dq.pop_front();
+            while(!dq.empty()&& nums[dq.back()]<=nums[i]) // if the back element is less than current element pop
+                dq.pop_back();
+            dq.push_back(i); // push one element at each iteration
+            if(i>=k-1)
+            res.push_back(nums[dq.front()]); // add the max element of each window
         }
 
         return res;
