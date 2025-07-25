3168e5b0c57479b84ea8364bb4da837d3032f743
b-jatin <59656324+b-jatin@users.noreply.github.com>
2025-07-25 10:12:14 -0700

    Time: 1 ms (46.51%) | Memory: 77.1 MB (43.49%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum=0,carry=0;
+        ListNode * dummy=new ListNode(-1);
+        ListNode * ans=dummy;
+        while(l1!=NULL || l2!=NULL || carry>0){
+            sum=carry;
+            if(l1){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            carry=sum/10;
+            dummy->next=new ListNode(sum%10);
+            dummy=dummy->next;
+        }
+        
+        return ans->next;
+        
+    }
+};
